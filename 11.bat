135cc3bd66d48c8d8a24fa68629f2ed82f109a9d
ildarikm <ildarik.m1982@yandex.ru>
2019-11-04 16:15:37 +0500

    Changed the HTML for the site.

---
+++ 11.bat
@@ -0,0 +1,6 @@
+echo "# my-first-blog" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git remote add origin https://github.com/ildarikm/my-first-blog.git
+ git push -u мастер оригинала
